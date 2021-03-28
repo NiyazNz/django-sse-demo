86fe06c69069a4f586ffc9712bba26d9330ddad8
Niyaz Batyrshin <Niyaz.Batyrshin@gmail.com>
2021-03-28 20:18:52 +0300

    Added examples description

--- README.md
+++ README.md
@@ -1,16 +1,20 @@
 # Django server sent events (SSE) demo
 
 Install requirements
+
 ```
 pip install -r requirements.txt
 ```
 
 Start server:
+
 ```
 uvicorn sse_demo.asgi:application
 ```
 
-Open browser on http://127.0.0.1:8000. Open dev tools console/network tab to 
-see messages.
+Open browser on http://localhost:8000. Open dev tools console/network tab to see
+messages.
 
 Open http://localhost:8000/message/ in a new tab to send messages.
+
+See [views.py](sse/views.py) for `ServerSentEventsResponse` usage examples.
--- sse/views.py
+++ sse/views.py
@@ -1,6 +1,5 @@
 import asyncio
 import json
-import uuid
 from datetime import datetime
 
 import aioredis
@@ -10,6 +9,8 @@ from django.http import JsonResponse, ServerSentEventsResponse
 from sse.channels.kafka import KafkaChannelsLayer
 from sse.channels.redis import RedisChannelsLayer
 from sse.channels.response import ChannelsSseResponse
+
+interval = 3
 
 
 async def send_view(request):
@@ -28,35 +29,67 @@ async def send_view(request):
     })
 
 
-async def time_ticker():
-    await asyncio.sleep(3)
+async def time_ticker_coro():  # coroutine function
+    await asyncio.sleep(interval)
     return datetime.now().isoformat()
 
 
+async def infinite_time_ticker():  # AsyncGenerator
+    while True:
+        await asyncio.sleep(interval)
+        yield datetime.now().isoformat()
+
+
+async def finite_time_ticker():
+    message_count = 5
+    for i in range(message_count):
+        await asyncio.sleep(interval)
+        yield f'{i+1}/{message_count}-{datetime.now().isoformat()}'
+    # EventSource client will reestablish connection and start new SSE.
+    # You should stop EventSource on client side.
+    yield 'EOF'
+
+
+# Example 1
+#   Receiving messages from awaitable function or AsyncGenerator
 async def background_task_sse_view(request):
     """Stream messages from background task"""
+    time_ticker = time_ticker_coro
+    # Alternatively AsyncGenerators can be used as a message source:
+    # time_ticker = infinite_time_ticker()
+    # time_ticker = finite_time_ticker()
+
     return ServerSentEventsResponse(time_ticker, last_event_id=request.headers.get('Last-Event-ID', 0))
 
 
+# Example 2
+#   Receiving messages from redis channel
 async def simple_redis_sse_view(request):
     """Stream messages from redis channel"""
     redis = await aioredis.create_redis(settings.REDIS_URL)
     channel, *_ = await redis.subscribe('sse')
 
-    response = ServerSentEventsResponse(channel.get_json, last_event_id=request.headers.get('Last-Event-ID', 0))
+    # `channel.get`, `channel.get_json` coroutine functions or `channel.iter()`
+    # AsyncIterator can be used as a message source
+    response = ServerSentEventsResponse(channel.get, last_event_id=request.headers.get('Last-Event-ID', 0))
     response.add_resource_closer(redis.close)
     return response
 
 
-# the same as simple_redis_sse_view
+# Example 3
+#   Receiving messages from redis channel.
+#
+#   Same as example 2 but delegates messages receiving and preprocessing to
+#   separate class implementing `receive` method - See `sse.channels.ChannelsLayer`.
+#   `ChannelsSseResponse` and `ChannelsLayer` should not be a part of MR and it
+#   is up to developer.
 async def redis_sse_view(request):
     """Stream messages from redis channel"""
     return ChannelsSseResponse(RedisChannelsLayer('sse'), last_event_id=request.headers.get('Last-Event-ID', 0))
 
 
+# Example 4
+#   Same as example 3 but uses kafka as a ChannelsLayer backend
 async def kafka_sse_view(request):
     """Stream messages from kafka topic"""
-    return ChannelsSseResponse(
-        KafkaChannelsLayer('sse', group_id=str(uuid.uuid4())),
-        last_event_id=request.headers.get('Last-Event-ID', 0)
-    )
+    return ChannelsSseResponse(KafkaChannelsLayer('sse'), last_event_id=request.headers.get('Last-Event-ID', 0))
